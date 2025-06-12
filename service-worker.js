e82093201078079260fcfcc4888e686934250b15
Thiemann423 <familie.thiemann@gmx.de>
2025-06-12 15:19:40 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,14 @@
-// Leerer Service Worker für PWA-Funktionalität
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(cacheNames.map(cache => caches.delete(cache)));
+    }).then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
+});
